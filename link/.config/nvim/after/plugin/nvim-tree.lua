d5b7dd2430be1743e4018c26834253c7dd5ed45a
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2023-01-15 06:24:35 +0800

    Changed remap for vimspector

--- link/.config/nvim/after/plugin/nvim-tree.lua
+++ link/.config/nvim/after/plugin/nvim-tree.lua
@@ -2,3 +2,8 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup()
+
+
+-- mappings
+vim.keymap.set('n', "<C-b>", vim.cmd.NvimTreeToggle)
+vim.keymap.set('n', "<leader>-", vim.cmd.NvimTreeFindFile)
