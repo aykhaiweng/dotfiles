c0f7162b11c784c6077c8a2e9f336e0823c58473
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2023-10-09 00:11:02 +0000

    feature: Swapping over to linuxbrew

--- init/23_ubuntu_install_kubectl.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Ubuntu-only stuff. Abort if not Ubuntu.
-is_ubuntu || return 1
-
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
