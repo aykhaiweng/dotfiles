82d3fbef0b4626d83ff9858ce8b7bffa614b44e0
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2023-11-23 08:50:13 +0800

    Delete init/42_install_fzf.sh

--- init/42_install_fzf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -d $HOME/.fzf ]]; then
-    e_arrow "FZF is already installed"
-else
-    e_success "Installing FZF"
-    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-    $HOME/.fzf/install --all
-fi
