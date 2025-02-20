8a5f43cc8da5a96e58d6fc7edce4d65d1cb7d48f
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2025-02-20 13:07:37 +0800

    feature: Cleaning nginx and nvim configs

--- init/44_clone_nginx_config.sh
+++ init/44_clone_nginx_config.sh
@@ -4,5 +4,5 @@ if [[ -d $HOME/.config/nvim ]]; then
     e_arrow "~/.config/nvim/ already exists"
 else
     e_success "Cloning nvim config to ~/.config/nvim/"
-    git clone --depth 1 git@github.com:aykhaiweng/nginx-conf.git $HOME/.config/nvim
+    git clone --depth 1 git@github.com:aykhaiweng/nvim-lua.git $HOME/.config/nvim
 fi
