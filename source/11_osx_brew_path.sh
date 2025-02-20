7c10d5906978074dac3ed885df512f048f25bf94
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2025-02-20 13:08:27 +0800

    feat: Removed unnecessary exports

--- source/11_osx_brew_path.sh
+++ source/11_osx_brew_path.sh
@@ -6,9 +6,6 @@ export PATH="/opt/homebrew/bin:$PATH"
 
 # libpq
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
-# export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
-# export LDFLAGS="-L/opt/homebrew/opt/libpq/lib"
-# export CPPFLAGS="-I/opt/homebrew/opt/libpq/include"
 
 # openssl
 export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"
