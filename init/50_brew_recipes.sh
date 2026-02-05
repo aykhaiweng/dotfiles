850a71d167c50e440124c29f35ed44728d8fb5cc
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-02-05 14:25:04 +0800

    conf: Added the configuration for binroot back

--- init/50_brew_recipes.sh
+++ init/50_brew_recipes.sh
@@ -75,10 +75,10 @@ local binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
 # fi
 
 # bash
-# if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
-#   e_header "Adding $binroot/bash to the list of acceptable shells"
-#   echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
-# fi
+if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
+  e_header "Adding $binroot/bash to the list of acceptable shells"
+  echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
+fi
 # if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
 #   e_header "Making $binroot/bash your default shell"
 #   sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
