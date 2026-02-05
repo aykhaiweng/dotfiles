d90080d4ceae4b67b8d31c5ef9cbc6b10c09f2e9
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-02-05 14:09:05 +0800

    rm: Removed more unneccessary configurations

--- init/50_brew_recipes.sh
+++ init/50_brew_recipes.sh
@@ -75,12 +75,12 @@ local binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
 # fi
 
 # bash
-if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
-  e_header "Adding $binroot/bash to the list of acceptable shells"
-  echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
-fi
-if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
-  e_header "Making $binroot/bash your default shell"
-  sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
-  e_arrow "Please exit and restart all your shells."
-fi
+# if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
+#   e_header "Adding $binroot/bash to the list of acceptable shells"
+#   echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
+# fi
+# if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
+#   e_header "Making $binroot/bash your default shell"
+#   sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
+#   e_arrow "Please exit and restart all your shells."
+# fi
