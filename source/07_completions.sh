1cc2359b4122d88012026a95186c3b35648a6046
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-02-14 23:23:37 +0000

    fmt: Added bash call

--- source/07_completions.sh
+++ source/07_completions.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # User completions
 # --- The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/linuxbrew/.linuxbrew/Caskroom/gcloud-cli/556.0.0/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/linuxbrew/.linuxbrew/Caskroom/gcloud-cli/556.0.0/google-cloud-sdk/completion.zsh.inc'; fi
