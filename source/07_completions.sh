c873b59a14964c9f9ac908c6c3b7916bdcb7e942
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-02-14 23:19:48 +0000

    refactor: Moved the completions to its own source file

--- source/07_completions.sh
+++ source/07_completions.sh
@@ -1,24 +1,3 @@
-# Path to your Oh My Zsh installation.
-export ZSH="$HOME/.oh-my-zsh"
-
-# Theme
-ZSH_THEME="af-magic"
-
-# Plugins
-plugins=(
-	brew
-	branch
-	fzf
-	git
-    git-auto-fetch
-	docker
-	docker-compose
-	zsh-syntax-highlighting
-	zsh-autosuggestions
-)
-
-source $ZSH/oh-my-zsh.sh
-
 # User completions
 # --- The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/linuxbrew/.linuxbrew/Caskroom/gcloud-cli/556.0.0/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/linuxbrew/.linuxbrew/Caskroom/gcloud-cli/556.0.0/google-cloud-sdk/completion.zsh.inc'; fi
@@ -26,8 +5,3 @@ if [ -f '/home/linuxbrew/.linuxbrew/Caskroom/gcloud-cli/556.0.0/google-cloud-sdk
 # --- The next line enables shell command completion for gcloud.
 if [ -f '/home/linuxbrew/.linuxbrew/Caskroom/gcloud-cli/556.0.0/google-cloud-sdk/path.zsh.inc' ]; then . '/home/linuxbrew/.linuxbrew/Caskroom/gcloud-cli/556.0.0/google-cloud-sdk/path.zsh.inc'; fi
 
-export GPG_TTY=$(tty)
-
-if [ -f ~/.bashrc ]; then
-  source ~/.bashrc
-fi
