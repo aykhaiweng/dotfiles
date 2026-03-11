818c1fb03b7255cf3ca881191f6a81f0365f3f82
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-03-11 13:55:56 +0800

    feat(source): wrap eza aliases in is_installed check
    Ensures eza aliases only override defaults if eza is available. Also performed minor cleanup in eza and lazygit config files.

--- source/04_eza.sh
+++ source/04_eza.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-export EZA_DEFAULT_OPTS="--color=always --long --git --icons=always --group-directories-first --time-style=long-iso"
+if is_installed eza; then
+  export EZA_DEFAULT_OPTS="--color=always --long --git --icons=always --group-directories-first --time-style=long-iso"
 
-alias ls="eza $EZA_DEFAULT_OPTS --no-user --no-permissions"
-alias ll="eza $EZA_DEFAULT_OPTS -la"
-alias llt="ll --tree"
-alias lt="ls --tree"
+  alias ls="eza $EZA_DEFAULT_OPTS --no-user --no-permissions"
+  alias ll="eza $EZA_DEFAULT_OPTS -la"
+  alias llt="ll --tree"
+  alias lt="ls --tree"
+fi
