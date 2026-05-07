cd932f7dd07701a5bab733044f9ab4e22d871c9d
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-05-08 05:41:31 +0800

    fix: Fixed issue with recipe forloop

--- init/50_brew_recipes.sh
+++ init/50_brew_recipes.sh
@@ -1,24 +1,26 @@
 # Exit if Homebrew is not installed.
-[[ ! "$(type -P brew)" ]] && e_error "Brew casks need Homebrew to install." && return 1
+[[ ! "$(type -P brew)" ]] && echo "Brew recipes need Homebrew to install." && return 1
 
-
-# Install Homebrew recipes.
 function brew_install_recipes() {
     local recipes=("$@")
     local installed_recipes
     installed_recipes=$(brew list --formula)
     local filtered_recipes=()
+
     for recipe in "${recipes[@]}"; do
+        # -q (quiet), -x (exact match), -F (fixed string)
         if ! echo "$installed_recipes" | grep -qxF "$recipe"; then
             filtered_recipes+=("$recipe")
         fi
     done
 
     if (( ${#filtered_recipes[@]} > 0 )); then
-        e_header "Installing Homebrew recipes: ${filtered_recipes[*]}"
+        echo "Installing Homebrew recipes: ${filtered_recipes[*]}"
         for recipe in "${filtered_recipes[@]}"; do
             brew install "$recipe"
         done
+    else
+        echo "All recipes are already installed."
     fi
 }
 
@@ -73,27 +75,14 @@ recipes=(
   "zsh"
 )
 
-brew_install_recipes
+# Pass the array to the function
+brew_install_recipes "${recipes[@]}"
 
-# Misc cleanup!
-
-# This is where brew stores its binary symlinks
-local binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
-
-# htop
-# if [[ "$(type -P $binroot/htop)" ]] && [[ "$(stat -L -f "%Su:%Sg" "$binroot/htop")" != "root:wheel" ]]; then
-#   e_header "Updating htop permissions"
-#   sudo chown root:wheel "$binroot/htop"
-#   sudo chmod u+s "$binroot/htop"
-# fi
+# Remove 'local' if not in a function
+binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
 
 # bash
-if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
-  e_header "Adding $binroot/bash to the list of acceptable shells"
+if [[ -f "$binroot/bash" ]] && ! grep -q "$binroot/bash" /etc/shells; then
+  echo "Adding $binroot/bash to the list of acceptable shells"
   echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
 fi
-# if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
-#   e_header "Making $binroot/bash your default shell"
-#   sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
-#   e_arrow "Please exit and restart all your shells."
-# fi
