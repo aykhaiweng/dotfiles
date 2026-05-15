bf724b3124fbccf00d35fcb760908013dd0c6f6a
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-05-15 13:12:08 +0800

    fix(brew): correct platform comment and set HOMEBREW_CACHE

--- source/10_brew.sh
+++ source/10_brew.sh
@@ -1,4 +1,5 @@
-# OSX-only stuff. Abort if not OSX.
+# Ubuntu-only stuff. Abort if not Ubuntu.
 is_ubuntu || return 1
 
+export HOMEBREW_CACHE="/mnt/packages/linuxbrew-cache"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
