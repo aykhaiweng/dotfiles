6d16ff01a555de68c207d4b8d90b83bedc7b17d6
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-05-15 07:52:20 +0000

    fix(brew): disable custom HOMEBREW_CACHE

--- source/10_brew.sh
+++ source/10_brew.sh
@@ -1,5 +1,5 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 is_ubuntu || return 1
 
-export HOMEBREW_CACHE="/mnt/packages/linuxbrew-cache"
+# export HOMEBREW_CACHE="/mnt/packages/linuxbrew-cache"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
